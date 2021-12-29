987388859e249ec38147232f6278d3cc464dd16e
wither7007 <jimsteffes@outlook.com>
2021-12-29 12:40:05 -0600

    ok

--- AdvancedVideoSearch/youtube.js
+++ AdvancedVideoSearch/youtube.js
@@ -1,4 +1,17 @@
+localStore = []
+// const notes = JSON.parse(localStorage.getItem('notes'))
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        text = target.textContent || target.innerText;
+    id = target.dataset.id
+    desc = target.dataset.desc
+    duration = target.dataset.duration
+    console.log(id)
+    localStore.push(id)
+    localStorage.setItem('notes', text)
 
+}, false);
 window.onload = function () {
     document.getElementById("subject").focus();
 }
@@ -9,6 +22,7 @@ document.getElementById('formS').addEventListener('submit', sumClick)
 url = ""
 function sumClick(event) {
     sub = document.getElementById('subject').value
+    sub = sub.replace(/\s+/g, '+');
     // alert(document.getElementById('subject').value)
     // alert("form submitted")
     event.preventDefault()
@@ -16,7 +30,7 @@ function sumClick(event) {
     // alert(sub)
     url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC4Yhlk-ADYBzSyiqD0QEgrvjB3plD4jao&part=snippet&q=${sub}&maxResults=5&publishedAfter=2021-12-21T02:49:05.100Z&publishedBefore=2021-12-21T02:49:05.100Z&order=relevance&videoDuration=long&type=video`
     console.log(url)
-    document.getElementById('test').innerHTML = url
+    // document.getElementById('test').innerHTML = url
     getYouTube(url)
 }
 apiKey = "AIzaSyC4Yhlk-ADYBzSyiqD0QEgrvjB3plD4jao"
@@ -26,7 +40,7 @@ xd = {}
 duration = ""
 // url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyC4Yhlk-ADYBzSyiqD0QEgrvjB3plD4jao&part=snippet&q=axiosjavascript&maxResults=5&publishedAfter=2021-12-21T02:49:05.100Z&publishedBefore=2021-12-21T02:49:05.100Z&order=relevance&videoDuration=long&type=video"
 function getYouTube(url) {
-    document.getElementById('test2').innerHTML = 'The thing that hath been, it is that which shall be; and that which is done is that which shall be done: and there is no new thing under the sun.'
+    // document.getElementById('mainItem').innerHTML = 'The thing that hath been, it is that which shall be; and that which is done is that which shall be done: and there is no new thing under the sun.'
     axios.get(url)
         .then(function (response) {
             // handle success
@@ -46,10 +60,17 @@ function getYouTube(url) {
                 .then(function (durRes) {
                     xd = durRes.data
                     duration = xd.items[0].contentDetails.duration
-                    document.getElementById('test2').innerHTML +=
-                        `Video id is : ${a.id.videoId}<hr> ${a.snippet.description}<br>
-                        ${lines.items[1].snippet.publishedAt}
-                        <br>Duration is ${duration} <br>  <a href="https://www.youtube.com/watch?v=${lines.items[1].id.videoId}" target="_blank">video</a>`
+                    document.getElementById('mainItem').innerHTML +=
+                        `<div class="col">
+                <div class="p-3 border bg-light" data-id=${a.id.videoId} data-desc=${a.snippet.description} data-duration=${duration}>
+                    https://www.youtube.com/watch?v=${a.id.videoId}<br>
+                    Video id is : ${a.id.videoId}
+                    <hr> ${a.snippet.description}<br>
+                    ${a.snippet.publishedAt}
+                    <br>Duration is ${duration} <br> <a href="https://www.youtube.com/watch?v=${a.id.videoId}"
+                        target="_blank">video</a>
+                </div>
+            </div> `
                     console.log("duration is " + duration)
                 })
         })
