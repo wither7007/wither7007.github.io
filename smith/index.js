4fb09ceafce52f778732a5c7b24d51d91a29c75f
wither7007 <jimsteffes@outlook.com>
2022-02-08 20:19:22 -0600

    ok

--- smith/index.js
+++ smith/index.js
@@ -1,66 +1,352 @@
-var age = 26;
-var beverage = (age >= 21) ? "Beer" : "Juice";
-console.log(beverage); // "Beer"
-var now = moment();
-var html = ""
-// console.log("moment date", now.format('dddd MMMM Do'));
-// console.log(`${now.add('3', 'days')}`)
-// console.log(`Adding three days: ${now.format('ll')}`);
+//get rid of colon on data
+let options = ""
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+  fetchWeather()
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+let places = []
+let pa = []
+function fetchWeather() {
+  //use the values from latitude and longitude to fetch the weather
+  let lat = 42.98
+  let lon = -81.23
+  let key = 'c1226a736dac2e52a2c456f0dc2c03ba';
+  let lang = 'en';
+  let units = 'imperial';
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+  //fetch the weather
+  fetch(url)
+    .then((resp) => {
+      if (!resp.ok) throw new Error(resp.statusText);
+      return resp.json();
+    })
+    .then((data) => {
+      showWeather(data);
+    })
+    .catch(console.err);
+}
+function showWeather(resp) {
+  // debugger
+  let row = document.querySelector('#weather');
+  //clear out the old weather and add the new
+  // row.innerHTML = '';
+  weather = resp.daily.map((day, idx) => {
+    if (idx <= 2) {
+      let dt = new Date(day.dt * 1000); //timestamp * 1000
+      // debugger
+      let sr = new Date(day.sunrise * 1000).toTimeString();
+      let ss = new Date(day.sunset * 1000).toTimeString();
+      // debugger
+      return `<div class="item">
+              ${dt.toDateString().substring(0, 3)}
+                   Low ${Math.trunc(day.temp.min)}&deg;
+                </div>
+              </div>
+            </div>
+          </div>`;
+    }
+  })
+    .join(' ');
+  row.innerHTML = weather
+}
+let serviceConvert = {
+  ch: "Crisis Hotline",
+  cc: "Clothes Closet",
+  me: "Meals",
+  fs: "Food Shelfs",
+  sh: "Shelter",
+  sw: "Showers",
+  th: "Rapid Rehousing",
+  ph: "Permanent Housing",
+  fu: "Furniture",
+  cm: "Case Management",
+  ea: "Energy Assitance",
+  ta: "Types of Asstisance",
+  cl: "Community Clinics",
+  hc: "Healthcare for Homeless",
+  uc: "Urgent Care",
+  st: "HIV & STD Testing & Treatment",
+  mh: "Mental Health",
+  cp: "Community Health",
+  aa: "Alcolhol and Chemcial Dependency",
+  de: "Dental",
+  vt: "Veterans",
+  ed: "Education",
+  jt: "Job Training",
+  tw: "Temporary Work",
+  yp: "Youth Programs",
+  im: "Services for Immigrants",
+  cr: "Victims of Crime and Abuse",
+  la: "Legal Aid",
+  pm: "Police Misconduct",
+  ad: "Advocacy",
+  ex: "Ex-offenders programs",
+  mf: "MN Family Investment Program (mfip)",
+  es: "Emergency Assistance",
+  sn: "Supplemental Nutrition Assistance Program (SNAP)",
+  ga: "General Assistance (GA)",
+  ma: "Medical Assistance (MA)",
+  mp: "Minnesota Supplemental Aid (MSA)",
+  ss: "Social Security",
+}
+//services for menu should be import
+let servicesMenu = {
+  Essentials: {
+    ch: "Hotlines/Emergency Phones",
+    cc: "Clothes Closet",
+    me: "Meals",
+    fs: "Food Shelf",
+    sh: "Shelter",
+    sw: "Showers",
+    th: "Rapid Housing",
+    ph: "Affordable Housing",
+    fu: "Furniture",
+    cm: "Case Management"
+  },
+  "Public Assistance": {
+    ta: "Types of Assistance",
+    ea: "Energy Assitance",
+    mf: "MN Family Investment Program (mfip)",
+    es: "Emergency Assistance",
+    sn: "Supplemental Nutrition Assistance Program (SNAP)",
+    ga: "General Assistance (GA)",
+    ma: "Medical Assistance (MA)",
+    mp: "Minnesota Supplemental Aid (MSA)",
+    ss: "Social Security",
+  },
+  "Health Care": {
+    cl: "Community Clinics",
+    hc: "Healthcare for Homeless",
+    uc: "Urgent Care",
+    st: "HIV & STD Testing & Treatment",
+    mh: "Mental Health",
+    cp: "Community Health",
+    aa: "Alcolhol and Chemcial Dependency",
+    de: "Dental",
+    vt: "Veterans"
+  },
+  "Education and Employment": {
+    ed: "Education",
+    jt: "Job Training",
+    tw: "Temporary Work"
+  },
+  "Special Help and Advocacy": {
+    yp: "Youth Programs",
+    im: "Services for Immigrants",
+    cr: "Victims of Crime and Abuse",
+    la: "Legal Aid",
+    pm: "Police Misconduct",
+    ad: "Advocacy",
+    ex: "Ex-offenders programs"
+  },
 
-const objArray = []
-var url = 'https://sheets.googleapis.com/v4/spreadsheets/1v0WTX_g0SEHb-EfG9faV3ayFo1WZUmUj8Lhgc2Kw2cA/values/menu?alt=json&key=AIzaSyCksSrPzSDpTmgJ-FaTT4_Xg6lHb9YtZJw'
-// console.log(moment().add(1, 'weeks').format("dddd [the] Do YYYY "))
-axios.get(url)
-  .then(function (response) {
-    // handle success
-    var xy = response.data
-    console.log(`xy is ${xy}`)
-    ObjtoHtml(xy)
-    console.log(xy);
+  "Developer": {
+    yx: "<a href='https://www.wandinround.com/story.html/' target='_blank' >About</a>",
+    yn: "<a href='https://www.wandinround.com/streets/form/' target='_blank' >Comments or Corrections</a>",
+    yp: "<a href='https://www.wandinround.com/nohome/' target='_blank' >A Song</a>",
+    lp: "<a href='https://donate.doctorswithoutborders.org/secure/donate' target='_blank' >Donate</a>",
+    sk: "<a href='https://docs.google.com/spreadsheets/d/1v0WTX_g0SEHb-EfG9faV3ayFo1WZUmUj8Lhgc2Kw2cA/edit#gid=1580880162' target='_blank' >Data</a>",
+  }
+}
+// const services = require('./cat.js')
+//populate slide menu
+let html = ` <ul id="slide-out" class="sidenav">
+`
+for (let f in servicesMenu) {
+  html += `
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="subheader">${f}</a></li>
+`
+  for (let p in servicesMenu[f]) {
+    html += `<li><a class="waves-effect ${p}" href="#!"> ${servicesMenu[f][p]
+      }</a></li>`
+  }
+}
+//populate collapsible menu
+html += "</ul>"
+document.getElementById("slide").innerHTML = html
+
+let htmlC = `<ul class="collapsible">`
+for (let f in servicesMenu) {
+  htmlC += `
+
+        <li>
+            <div class="collapsible-header"><i class="material-icons">expand_more</i>${f}</div>
+`
+  for (let p in servicesMenu[f]) {
+    htmlC += `<div class="collapsible-body"><span class="${p}">${servicesMenu[f][p]
+      }</span></div>`
+  }
+
+  htmlC += "</li>"
+}
+htmlC += "</ul>"
+document.getElementById("collapse").innerHTML = htmlC
+
+//search on change
+function filt(item) {
+  // debugger
+  let sear = document.getElementById("search").value.toUpperCase()
+
+  if (
+    item[0].toUpperCase().match(sear) ||
+    serviceConvert[item[8]].toUpperCase().match(sear)
+  ) {
+    return true
+  }
+}
+
+function filChar(char) {
+  let cards = document.getElementById("cards")
+  places = allPlaces.filter(filt)
+  let str = mapper(places)
+  cards.innerHTML = str
+}
+//generic fill card mapper
+function mapper(aPlaces) {
+  let p = aPlaces
+    .map(
+      f =>
+        `<div class="row ${f[10] === "h" ? "special" : 3}" style="height: 95 %">
+                <div class="card blue-grey darken-1 " style="height: 95%">
+                    <div class="card-content white-text">
+                        <span class="card-title">${f[0]}
+                        </span></span>
+                      Service: ${serviceConvert[f[8]]} <br>
+                      Address: ${f[11]} <br>
+                        Phone: ${f[2]} <br>
+                        Hours: ${f[3]} <br>
+                        Remarks: ${f[4]} <br>
+                        Google rating: ${f[6]} <br>
+                        <a class="myButton" style="margin-top:20px;" href="https://www.google.com/maps/search/?api=1&query=${f[0].replace(
+          /\s+/g,
+          "+"
+        )}+Minneapolis" target="_blank">Directions </a>
+                        
+                       <a class="myButton" style="margin-top:20px; margin-left:10px" href="${f[15]}" target="_blank">website </a>  
+                       <a class="myButton" style="margin-top:20px; margin-left:8px"href="tel:${f[2].replace(/-/g, "")}" id="call" target="_blank">Call </a>  
+                        </div>
+                        
+
+            </div>
+        </div>`
+    )
+    .join("")
+  return p
+}
+//menu fill by type
+function filType(Aplaces, type) {
+  let cards = document.getElementById("cards")
+  let q
+  places = Aplaces.filter(q => q[8] === type)
+  let str = mapper(places)
+  cards.innerHTML = str
+}
+// url = 'https://raw.githubusercontent.com/wither7007/handbook/master/data/data.json'
+// url = "./local.json"
+let url = "data.json"
+let allPlaces = []
+fetch(url)
+  .then(resp => resp.json())
+  .then(function (data) {
+    allPlaces = allPlaces.concat(data)
+    filType(allPlaces, "sh")
   })
 
-function ObjtoHtml(xy) {
-  // xy.values.forEach(a => console.log(a[1]))
-  // rValues = xy.values
-  debugger
-  xy.values.forEach((a, i) => {
-    console.log(`a is ${a}`)
-    html += `
-            <div class="accordion" id="accordionExample">
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="headingOne">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}"
-          aria-expanded="true" aria-controls="collapseOne">
-          ${moment().add(i, 'days').format('dddd MMMM Do')}
-        </button>
-      </h2>
-      <div id="collapse${i}" class="accordion-collapse collapse ${i == 0 ? 'show' : ''} 
-      " aria-labelledby="headingOne"
-        data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-        
-        <b>Meals</b>
-        <br>
-          ${a[0]}
-          <br>
-         ${a[1]}
-         <br>
-         ${a[2]}
-          <hr>
-          <b>Trips</b>
-          <br>
-                   ${a[3]}
-          <br>
-                   ${a[4]}
-        </div>
-      </div>
-    </div>
+for (let sc in serviceConvert) {
+  let p = document.getElementsByClassName(sc)
+  for (q of p) {
+    q.addEventListener("click", function () {
+
+      // debugger
+      filType(allPlaces, sc)
+    })
+  }
+}
+
+function removeTop() {
+  // document.querySelector('.top').style.display = "none"
+  document.querySelector('#second').style.height = "85vh"
+}
+Array.from(document.querySelectorAll('.collapsible-header')).forEach(
+  ele => ele.addEventListener('click', removeTop)
+)
+document.getElementById('print').addEventListener('click', print)
+//set date function
+let now = new Date();
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let day = days[now.getDay()];
+let month = months[now.getMonth()];
+let greeting = () => {
+  var today = new Date()
+  var curHr = today.getHours()
+
+  if (curHr < 12) {
+    return ('Good Morning')
+  } else if (curHr < 18) {
+    return ('Good Afternoon!')
+  } else {
+    return ('Good Evening')
+  }
 
 
-          `
+
+}
+document.getElementById('today').innerHTML = `${greeting()}  Today is:  ${day} ${month} ${now.getDate()}, ${now.getFullYear()}`
+
+function print() {
+  pa = []
+  let line = {
+    0: "---------------",
+    1: "",
+    2: "",
+    3: "",
+    4: ""
+  }
+  let placesSlice = places.map(x => [x[0], x[11], x[3], x[2], x[4]])
+  placesSlice.forEach(x => {
+    pa.push(
+      x.reduce(function (acc, cur, i) {
+        acc[i] = cur
+        return acc
+      }, {})
+    )
+    pa.push(line)
   })
-  console.log(html)
-  var hText = document.getElementById('replace')
-  hText.innerHTML = html
-
+  printJS({
+    printable: pa,
+    documentTitle: serviceConvert[places[0][8]],
+    properties: [{
+      field: "0",
+      displayName: "Place",
+    },
+    {
+      field: "1",
+      displayName: "Address"
+    },
+    {
+      field: "2",
+      displayName: "Hours"
+    },
+    {
+      field: "3",
+      displayName: "Phone"
+    },
+    {
+      field: "4",
+      displayName: "Remarks"
+    }
+    ],
+    type: "json",
+    gridStyle: "border: 2px solid #3971A5;"
+  }, 'tim')
 }
--- smith/node.js
+++ smith/node.js
@@ -3,12 +3,19 @@ import fetch from 'node-fetch';
 // fetch('https://google.com')
 //     .then(res => res.text())
 //     .then(text => console.log(text));
-url = 'api.openweathermap.org/data/2.5/weather?lat=45.0195&lon=-93.420&appid=c1226a736dac2e52a2c456f0dc2c03ba'
-fetch('https://jsonplaceholder.typicode.com/users')
+let lat = 45.01977340933548
+let lon = -93.42030115198955
+let key = 'c1226a736dac2e52a2c456f0dc2c03ba';
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units="imperial"&appid=${key}`
+console.log(url)
+fetch(url)
     .then(res => res.json())
     .then(json => {
-        console.log("First user in the array:");
-        console.log(json[0]);
-        console.log("Name of the first user in the array:");
-        console.log(json[0].name);
+        console.log(json)
+        console.log(json.main)
+        // console.log("First user in the array:");
+        // console.log(json[0]);
+        // console.log("Name of the first user in the array:");
+        // console.log(json[0].name);
     })
+// url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city, key)
