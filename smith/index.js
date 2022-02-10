be21726f04cb77fc746523d6864c30251f8c310a
wither7007 <jimsteffes@outlook.com>
2022-02-10 00:24:22 -0600

    ok

--- smith/index.js
+++ smith/index.js
@@ -1,20 +1,30 @@
 var now = moment();
 let lat = 45.01977340933548
 let lon = -93.42030115198955
+let units = "imperial"
+let lang = 'en'
 let wKey = 'c1226a736dac2e52a2c456f0dc2c03ba';
 let sKey = 'AIzaSyCksSrPzSDpTmgJ-FaTT4_Xg6lHb9YtZJw'
 let Turl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units='imperial'&appid=${wKey}`
-
+let Durl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units='imperial'&appid=${wKey}`
+// let Durl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${wKey}&units=${units}&lang=${lang}`;
 var url = `https://sheets.googleapis.com/v4/spreadsheets/1v0WTX_g0SEHb-EfG9faV3ayFo1WZUmUj8Lhgc2Kw2cA/values/menu?alt=json&key=${sKey}`
 var kelvin = 0
 const requestOne = axios.get(Turl);
 const requestTwo = axios.get(url);
-axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+const requestThree = axios.get(Durl);
+axios.all([requestOne, requestTwo, requestThree]).then(axios.spread((...responses) => {
   const responseOne = responses[0].data
+
   const responseTwo = responses[1].data
-  // debugger
+  console.log(responseTwo)
+  const responseThree = responses[2].data
+
+
+
+  debugger
   // rText(responseOne, responseTwo)
-  ObjtoHtml(responseTwo, responseOne)
+  ObjtoHtml(responseTwo, responseOne, responseThree)
   // console.log(responseOne.data)
   // console.log(responseTwo.data.values)
   me = (responseTwo.data.values)
@@ -27,57 +37,27 @@ axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
 
 
 // const requestOne = axios.get(TemUrl);
-const requestWeather = axios.get(Turl);
+// const requestWeather = axios.get(Turl);
 function ktof(k) {
   var one = k - 273.5
   var two = one * (9 / 5)
   var three = two + 32
   return Math.round(three)
 }
-function temp() {
-  fetch(Turl)
-    .then(res => res.json())
-    .then(json => {
-      // console.log(json)
-      var kelvin = json.main.temp
-      // console.log(kelvin)
-      var far = Math.round(ktof(kelvin))
-
-      console.log(`far is in TURL ${far}`)
-      // console.log(`Farenheit is ${far} `)
-      return far
-    })
-  console.log(`kelvin is ${kelvin}`)
-}
 var html = ""
 // console.log("moment date", now.format('dddd MMMM Do'));
 // console.log(`${now.add('3', 'days')}`)
 // console.log(`Adding three days: ${now.format('ll')}`);
 
-async function getWeatherAsync() {
-  let response = await fetch(Turl);
-  let data = await response.json()
-  return data;
-}
 
-getWeatherAsync()
-  .then(data => me = data);
 
 const objArray = []
 // console.log(moment().add(1, 'weeks').format("dddd [the] Do YYYY "))
-axios.get(url)
-  .then(function (response) {
-    // handle success
-    var xy = response.data
-    console.log(`xy is ${xy}`)
-    var zz = xy.values
-    ObjtoHtml(xy)
-    console.log(xy);
-  })
 
-function ObjtoHtml(xy, zz) {
+function ObjtoHtml(xy, zz, yy) {
   // xy.values.forEach(a => console.log(a[1]))
   // rValues = xy.values
+  debugger
   xy.values.forEach((a, i) => {
     // console.log(`a is ${a}`)
     debugger
