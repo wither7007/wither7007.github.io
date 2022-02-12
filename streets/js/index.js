2fa60b31e42c1d0df8f223dc0002ab52238b3cc3
wither7007 <jimsteffes@outlook.com>
2022-02-12 10:08:38 -0600

    ok

--- streets/js/index.js
+++ streets/js/index.js
@@ -11,52 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 let places = []
 let pa = []
-function fetchWeather() {
-  //use the values from latitude and longitude to fetch the weather
-  // let lat = 42.98
-  // let lon = -81.23
-  let lat = 45.01977340933548
-  let lon = -93.42030115198955
-  let key = 'c1226a736dac2e52a2c456f0dc2c03ba';
-  let lang = 'en';
-  let units = 'imperial';
-  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
-  //fetch the weather
-  fetch(url)
-    .then((resp) => {
-      if (!resp.ok) throw new Error(resp.statusText);
-      return resp.json();
-    })
-    .then((data) => {
-      showWeather(data);
-    })
-    .catch(console.err);
-}
-function showWeather(resp) {
-  // debugger
-  let row = document.querySelector('#weather');
-  //clear out the old weather and add the new
-  // row.innerHTML = '';
-  weather = resp.daily.map((day, idx) => {
-    if (idx <= 2) {
-      let dt = new Date(day.dt * 1000); //timestamp * 1000
-      // debugger
-      let sr = new Date(day.sunrise * 1000).toTimeString();
-      let ss = new Date(day.sunset * 1000).toTimeString();
-      // debugger
-      return `<div class="item">
-              ${dt.toDateString().substring(0, 3)}
-                   Low ${Math.trunc(day.temp.min)}&deg;
-                </div>
-              </div>
-            </div>
-            <small class="yellow-text"> Test release </small>
-          </div>`;
-    }
-  })
-    .join(' ');
-  row.innerHTML = weather
-}
 let serviceConvert = {
   ch: "Crisis Hotline",
   cc: "Clothes Closet",
@@ -148,12 +102,19 @@ let servicesMenu = {
     ex: "Ex-offenders programs"
   },
 
-  "Developer": {
+  "<b><u>Developer</u></b>": {
+
+    qq: "this is qq",
+    q1: "this is qq",
+    q2: "this is qq",
+    qq: "this is qq",
     yx: "<a href='https://www.wandinround.com/story.html/' target='_blank' >About</a>",
     yn: "<a href='https://www.wandinround.com/streets/form/' target='_blank' >Comments or Corrections</a>",
     yp: "<a href='https://www.wandinround.com/nohome/' target='_blank' >A Song</a>",
     lp: "<a href='https://donate.doctorswithoutborders.org/secure/donate' target='_blank' >Donate</a>",
     sk: "<a href='https://docs.google.com/spreadsheets/d/1v0WTX_g0SEHb-EfG9faV3ayFo1WZUmUj8Lhgc2Kw2cA/edit#gid=1580880162' target='_blank' >Data</a>",
+    q5: "this is qq",
+    q6: "this is qq"
   }
 }
 // const services = require('./cat.js')
@@ -260,12 +221,14 @@ fetch(url)
   .then(resp => resp.json())
   .then(function (data) {
     allPlaces = allPlaces.concat(data)
-    filType(allPlaces, "sh")
+    filType(allPlaces, "ch")
   })
 
 for (let sc in serviceConvert) {
+  // console.log(sc)
   let p = document.getElementsByClassName(sc)
   for (q of p) {
+    console.log(`q of p is ${q}`)
     q.addEventListener("click", function () {
 
       // debugger
