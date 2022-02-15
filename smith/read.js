f502eb2c4ae551146d29e24156dcdb151395229a
wither7007 <jimsteffes@outlook.com>
2022-02-15 11:50:30 -0600

    ok

--- smith/read.js
+++ smith/read.js
@@ -1,21 +1,26 @@
-const filename = "/projects/wither7007.github.io/streets/data.json"
+import fetch from 'node-fetch';
+
+// const response = await fetch('https://api.github.com/users/github');
+// const data = await response.json();
+
+// console.log(data);
+const filename = "style.css"
 // process.exit()
 console.log(filename)
-// fetch(filename)
-//     .then(response => response.text())
-//     .then(data => {
-//         // Do something with your data
-//         console.log(data);
-//     });
-// const fs = require('fs')
+fetch(filename)
+    .then(response => response.text())
+    .then(data => {
+        // Do something with your data
+        console.log(data);
+    });
 
 // fs.readFile(filename)
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log(data)
-})
+// fs.readFile(filename, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     console.log(data)
+// })
