76616415a2791ddffc4b5b3bb6162b76cbd1b1c8
wither7007 <jimsteffes@outlook.com>
2022-02-15 10:52:23 -0600

    ok

--- smith/README.markdown
+++
@@ -1,5 +0,0 @@
-# Bootstrap 4 Responsive Calendar
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/chrisdpratt/pen/OOybam](https://codepen.io/chrisdpratt/pen/OOybam).
-
-
--- smith/last.js
+++
@@ -1,9 +0,0 @@
-// print last record for and data.json and noG.json
-k = require("/mnt/c/projects/perm/hand/data/data.json")
-l = require("/mnt/c/projects/perm/hand/data/noG.json")
-debugger
-console.log("/mnt/c/projects/perm/hand/data/noG.json")
-console.log(l[l.length - 1])
-console.log("/mnt/c/projects/perm/hand/data/data.json")
-console.log(k[k.length - 1])
-console.log(k[k.length - 1][9])
