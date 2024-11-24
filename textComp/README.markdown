d5687902dbdaba7db7555c0abac40ffabd0896f7
jim steffes <jimsteffes@outlook.com>
2024-11-24 16:33:06 -0600

    more clean

--- textComp/README.markdown
+++
@@ -1,5 +0,0 @@
-# Gap Property
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/seyedi/pen/YzwmaBY](https://codepen.io/seyedi/pen/YzwmaBY).
-
-https://css-tricks.com/almanac/properties/g/gap
--- textComp/script.js
+++
@@ -1,6 +0,0 @@
-var container = document.querySelector('.container'),
-    slider = document.querySelector('input');
-
-slider.addEventListener('input', function () {
-  container.style.gap = this.value + 'rem';
-}, false);    
